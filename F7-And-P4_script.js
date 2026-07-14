fccb70cd288951319ed49ebe51426d545b61ad21
smsisonz <smsison1@feualabang.edu.ph>
2026-07-14 21:45:46 +0800

    Added the remaning html files and Js files
    files needed for a fully functional-ish website that has the other html files in it.
    as well as the dark mode toggle.

---
+++ F7-And-P4_script.js
@@ -0,0 +1,18 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        darkModeBtn.textContent = "☀️ Light Mode";
+
+    }
+    else{
+
+        darkModeBtn.textContent = "🌙 Dark Mode";
+
+    }
+
+});
